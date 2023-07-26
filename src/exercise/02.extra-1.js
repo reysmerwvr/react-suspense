10faba9f1c50d2c332f7db9b9811bc36aefa6de2
Reysmer Valle <reysmerwvr@gmail.com>
2023-07-26 21:58:52 +0200

    complete render as you fetch exercises

--- src/exercise/02.extra-1.js
+++ src/exercise/02.extra-1.js
@@ -51,22 +51,26 @@ function App() {
     <div className="pokemon-info-app">
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
-      <div className="pokemon-info">
-        {pokemonResource ? (
-          <PokemonErrorBoundary
-            onReset={handleReset}
-            resetKeys={[pokemonResource]}
-          >
-            <React.Suspense
-              fallback={<PokemonInfoFallback name={pokemonName} />}
+      <React.Suspense
+        fallback={
+          <div className="pokemon-info">
+            <PokemonInfoFallback name={pokemonName} />
+          </div>
+        }
+      >
+        <div className="pokemon-info">
+          {pokemonResource ? (
+            <PokemonErrorBoundary
+              onReset={handleReset}
+              resetKeys={[pokemonName]}
             >
               <PokemonInfo pokemonResource={pokemonResource} />
-            </React.Suspense>
-          </PokemonErrorBoundary>
-        ) : (
-          'Submit a pokemon'
-        )}
-      </div>
+            </PokemonErrorBoundary>
+          ) : (
+            'Submit a pokemon'
+          )}
+        </div>
+      </React.Suspense>
     </div>
   )
 }
